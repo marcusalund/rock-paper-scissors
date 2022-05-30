4628f770b1349cd7053df18966f3dc8754e3489e
Marcus Lund <marcusalund@gmail.com>
2022-05-29 22:10:37 -0600

    Create playSingleRound function

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function computerPlay (){
         return "Scissors";
     }
 }
+
+function playSingleRound(playerSelection, computerSelection){
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    
+    if (playerSelection === "Rock" && computerSelection === "Scissors"){
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+        return "You win! Scissors beats Paper!";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper"){
+        return "You lost! Paper beats Rock!";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+        return "You lost! Rock beats Scissors!";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors"){
+        return "You lost! Scissors beats Paper!";
+    } else {
+        return "It's a tie!! " + playerSelection + " and " + computerSelection;
+    }
+}
+
+//let playerSelection = prompt("Rock, Paper, or Scissors...which do you choose?").toLowerCase();
+//let computerSelection = computerPlay();
